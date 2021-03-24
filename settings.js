87cd618e7301e188f9a669643b9a6412b7204ee3
mariosath <Athmarios@agenso.gr>
2021-03-25 00:36:47 +0200

    Added modal,settings panel, animations and more.

--- settings.js
+++ settings.js
@@ -1,16 +1,12 @@
-// $(document).ready(function() {
-// });
-
-  $(".btn").click(
-    function() {
-
+$(".btn").click(
+  function() {
+    // $('.active').fadeOut('slow', function() {
       if ( $(".home-panel").hasClass("active") ) {
         $(".settings-panel").addClass("active");
         $(".home-panel").removeClass("active");
       } else {
-        $(".settings-panel").addClass("active");
-        $(".home-panel").removeClass("active");
+        $(".settings-panel").removeClass("active");
+        $(".home-panel").addClass("active");
       }
-
-
-    });
+    // })
+  });
